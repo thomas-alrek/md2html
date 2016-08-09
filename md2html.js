a1fca68b0ec10c8758ef340928305f2813f2de6b
Thomas Alrek <thomas@alrek.no>
2016-08-09 23:42:22 +0200

    Implemented more of the spec

--- md2html.js
+++ md2html.js
@@ -4,8 +4,10 @@
 function md2html(md){
     /* regex patterns */
     var newline_pattern = /\n/gim;
-    var header_pattern = /^(#{1,6}) (.+)/gim;  // <h1> -> <h6>
-    var header_fix = /(\<\/h[1-6]\>)(\r|\n|\r\n)?\<br\>/gim;
+    var header_pattern1 = /^(#{1,6}) (.+)(\n|\r)/gim;  // <h1> -> <h6>
+    var header_pattern2 = /^(.+)(\n|\r)(=+)(\n|\r)/gim; // <h1>
+    var header_pattern3 = /^(.+)(\n|\r)(-+)(\n|\r)/gim; // <h2>
+    //var header_fix = /(\<\/h[1-6]\>)(\r|\n|\r\n)?\<br\>/gim;
     var bold_pattern1 = /\*{2}(.+)\*{2}/gim; // <b>
     var bold_pattern2 = /\_{2}(.+)\_{2}/gim;  // <b>
     var italic_pattern1 = /\_(.+)\_/gim; // <i>
@@ -15,10 +17,39 @@ function md2html(md){
     var a_pattern1 = /\[(.+)\]\((.+)\)/gim; // <a>
     var a_pattern2 = /\[(.+)\]\((.+) \"(.+)\"\)/gim; // <a>
     var a_pattern3 = /\[(.+)\]/gim; // <a>
-        
+    //var a_pattern4 = /\<(.+)\>/gim;
+    
+    /* links */
+    /*md = md.replace(a_pattern4, function(match, url){
+        return '<a href="' + url + '">' + url + '</a>';
+    });*/
+    md = md.replace(a_pattern1, function(match, title, url){
+        return '<a href="' + url + '">' + title + '</a>';
+    });
+    md = md.replace(a_pattern2, function(match, title, url, tooltip){
+        return '<a href="' + url + '" title="' + tooltip + '">' + title + '</a>';
+    });
+    md = md.replace(a_pattern3, function(match, url){
+        return '<a href="' + url + '">' + url + '</a>';
+    });
+
     /* headers */
-    md = md.replace(header_pattern, function(match, size, str){
+    md = md.replace(header_pattern1, function(match, size, str){
         return '<h' + size.length + '>' + str + '</h' + size.length + '>';
+    });
+    md = md.replace(header_pattern2, function(match, str, _, size, __){
+        if(size.length === str.length){
+            return '<h1>' + str + '</h1>';
+        }else{
+            return match;
+        }
+    });
+    md = md.replace(header_pattern3, function(match, str, _, size, __){
+        if(size.length === str.length){
+            return '<h2>' + str + '</h2>';
+        }else{
+            return match;
+        }
     });
     
     /* bold */
@@ -42,25 +73,14 @@ function md2html(md){
         return '<del>' + str + '</del>';
     });
     
-    /* links */
-    md = md.replace(a_pattern1, function(match, title, url){
-        return '<a href="' + url + '">' + title + '</a>';
-    });
-    md = md.replace(a_pattern2, function(match, title, url, tooltip){
-        return '<a href="' + url + '" title="' + tooltip + '">' + title + '</a>';
-    });
-    md = md.replace(a_pattern3, function(match, url){
-        return '<a href="' + url + '">' + url + '</a>';
-    });
-    
     /* newline */
     md = md.replace(newline_pattern, function(match, str){
         return '<br>';
     });
     
-    md = md.replace(header_fix, function(match, header){
+    /*md = md.replace(header_fix, function(match, header){
         return header;
-    });
+    });*/
     
     return md;
 }
